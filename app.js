c1b83682a70f5358b1110518086005ee19c7d2ee
dean <nicklasfrederiksen@gmail.com>
2022-08-23 15:59:35 +0200

    First

--- app.js
+++ app.js
@@ -15,14 +15,34 @@ app.use(cors({
 }));
 
 app.get('/data',(req,response) => {
-  //Add simple mysql or mysql2 example
-  response.json("Data from server!");
-});
+  const connection = mysql.createConnection({
+    host:process.env["DB.HOST"],
+    user:process.env["DB.USER"],
+    password:process.env["DB.PASS"],
+    dbname:process.env["DB.DBNAME"],
+    ssl: {
+      "rejectUnauthorized":true
+    }
+  });
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
+  const sql = `SELECT * FROM spotify LIMIT 10`
+
+  connection.query(sql, (error, results) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    response.send(results);
+    console.log(results);
+  });
+});
 
 server.listen(3000,(port) => {
   console.log('Listening on port ' + server.address().port);
-});
 
+});
 
 module.exports = app;

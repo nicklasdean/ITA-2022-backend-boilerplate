7a17c2898f00dcaeccc0d905511ca68c0153ae07
Benjamin Hughes <behu@kea.dk>
2022-08-23 14:05:22 +0200

    Changed port, added cors and send different response

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+var http = require('http');
 const path = require('path');
 const mysql = require('mysql');
+const cors = require('cors');
 
-const app = express();
+var app = express();
+var server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/data',(req,response) => {
   //Add simple mysql or mysql2 example
-  response.send("asd");
+  response.json("Data from server!");
 });
 
-app.listen(8000,() => {
+
+server.listen(3000,(port) => {
+  console.log('Listening on port ' + server.address().port);
 });
 
 

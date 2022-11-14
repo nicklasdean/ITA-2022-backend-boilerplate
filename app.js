c9d7fa395d883987b6d12eb0d8f134bc6d41aea2
dean <nicklasfrederiksen@gmail.com>
2022-11-14 14:10:36 +0100

    First

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
+
 app.use(cors({
   origin: '*'
 }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worldasdæisdouhdsohuadsadspiads!')
 });
 
-app.get('/data',(req,response) => {
-  const connection = mysql.createConnection({
-    host:process.env["DB_HOST"],
-    user:process.env["DB_USER"],
-    password:process.env["DB_PASS"],
-    dbname:process.env["DB_NAME"],
-    ssl: {
-      "rejectUnauthorized":true
-    }
-});
+const connection = mysql.createConnection({
+  host: process.env["DB_HOST"],
+  user: process.env["DB_USER"],
+  password: process.env["DB_PASS"],
+  database: process.env["DB_NAME"],
+  ssl: {
+    "rejectUnauthorized": this.host === "localhost"
+  }
+})
 
-  connection.connect(function(err) {
+connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
 
-const sql = `SELECT * FROM spotify LIMIT 10`
 
-connection.query(sql, (error, results) => {
-    response.send(results);
-    console.log(results);
+app.get('/data',(req, response) => {
+  const sql = `SELECT * FROM spotify LIMIT 10`;
+  connection.query(sql, (err,rows) => {
+    response.send(rows);
   });
 });
 

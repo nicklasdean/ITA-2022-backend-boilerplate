2e82098ed011d27d83978c4e609733422a9ca678
dean <nicklasfrederiksen@gmail.com>
2022-11-16 16:50:44 +0100

    added new branch + POST

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
+
+const connection = require('./database/config');
 
 app.use(express.json());
 
@@ -15,22 +16,6 @@ app.use(cors({
 app.get('/', (req, res) => {
   res.send('Hello ITA!')
 });
-
-const connection = mysql.createConnection({
-  host: process.env["DB_HOST"],
-  user: process.env["DB_USER"],
-  password: process.env["DB_PASS"],
-  database: process.env["DB_NAME"],
-  ssl: {
-    "rejectUnauthorized": this.host === "localhost"
-  }
-})
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
 
 app.get('/data',(req, response) => {
   const sql = `SELECT * FROM spotify LIMIT 10`;
---
+++ database/config.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+require('dotenv').config()
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: {
+        "rejectUnauthorized": this.host === "localhost"
+    }
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+module.exports = connection;
